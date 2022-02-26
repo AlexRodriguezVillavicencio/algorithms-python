c3c1e021065ef7c7eebee89418b0b7469ef2866c
Alex <arodriguezv@uni.pe>
2022-02-26 01:51:22 -0500

    prueb3

--- prueba3.py
+++ prueba3.py
@@ -1,6 +1,18 @@
 
 def NumeroCapicua(numero):
-    global total
+    '''
+    En matemáticas, la palabra capicúa (del catalán cap i cua, 'cabeza y cola')​ 
+    se refiere a cualquier número que se lee igual de izquierda a derecha que 
+    de derecha a izquierda. Se denominan también números palíndromos.
+    Esta función devuelve el valor booleano True si el número es capicúa, de lo contrario
+    devuelve el valor booleano False 
+    En caso de que el parámetro no sea de tipo entero, debe retornar nulo.
+    Recibe un argumento:
+        numero: Será el número sobre el que se evaluará si es capicúa o no lo es.
+    Ej:
+        NumeroCapicua(787) debe retornar True
+        NumeroCapicua(108) debe retornar False
+    '''
     # Tu código aca:
     lista= []
     if isinstance(numero,int):
@@ -18,6 +30,6 @@ def NumeroCapicua(numero):
 
     return resultado
 
-print(NumeroCapicua(232)) 
+print(NumeroCapicua(23232)) 
 print(NumeroCapicua(132)) 
 print(NumeroCapicua('hola')) 
