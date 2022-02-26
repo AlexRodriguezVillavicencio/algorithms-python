588469c1d8a64248e51c51915fb756127d9391f6
Alex <arodriguezv@uni.pe>
2022-02-26 01:57:39 -0500

    prueb4

--- prueba4.py
+++ prueba4.py
@@ -1,33 +1,25 @@
-def NumeroCapicua(numero):
+def Factorial(numero):
     '''
-    En matemáticas, la palabra capicúa (del catalán cap i cua, 'cabeza y cola')​ 
-    se refiere a cualquier número que se lee igual de izquierda a derecha que 
-    de derecha a izquierda. Se denominan también números palíndromos.
-    Esta función devuelve el valor booleano True si el número es capicúa, de lo contrario
-    devuelve el valor booleano False 
-    En caso de que el parámetro no sea de tipo entero, debe retornar nulo.
+    Esta función devuelve el factorial del número pasado como parámetro.
+    En caso de que no sea de tipo entero y/o sea menor que 1, debe retornar nulo.
     Recibe un argumento:
-        numero: Será el número sobre el que se evaluará si es capicúa o no lo es.
+        numero: Será el número con el que se calcule el factorial
     Ej:
-        NumeroCapicua(787) debe retornar True
-        NumeroCapicua(108) debe retornar False
+        Factorial(4) debe retornar 24
+        Factorial(-2) debe retornar nulo
     '''
     #Tu código aca:
-    lista= []
-    if isinstance(numero,int):
-        arreglo = list(str(numero))
-        for i in reversed(arreglo):
-            lista.append(i)
-        invertido = "".join(lista)
-
-        if(int(invertido) == int(numero)):
-            resultado = True
-        else:
-            resultado = False 
-    
+    if isinstance(numero,int) and numero >= 1:
+        multiplicar = 1
+        for i in range(1,numero+1):
+            multiplicar *=i
+        resultado = multiplicar
+    else:
+        resultado = None
 
     return resultado
+
    
 
         
-print(NumeroCapicua(323)) 
+print(Factorial(-2)) 
