ddf3e000f35fda259b2c03ae5461674e9c3e42a3
Alex <arodriguezv@uni.pe>
2022-02-26 05:22:06 -0500

    prueba8

---
+++ prueba7.py
@@ -0,0 +1,26 @@
+def ClaseAnimal(especie, color):
+    '''
+    Esta función devuelve un objeto instanciado de la clase Animal, 
+    la cual debe tener los siguientes atributos:
+        Edad    (Un valor de tipo de dato entero, que debe inicializarse en cero)
+        Especie (Un valor de tipo de dato string)
+        Color   (Un valor de tipo de dato string)
+    y debe tener el siguiente método:
+        CumplirAnios  (este método debe sumar uno al atributo Edad y debe devolver ese valor)
+    Recibe dos argumento:
+        especie: Dato que se asignará al atributo Especie del objeto de la clase Animal
+        color: Dato que se asignará al atributo Color del objeto de la clase Animal
+    Ej:
+        a = ClaseAnimal('perro','blanco')
+        a.CumpliAnios() -> debe devolver 1
+        a.CumpliAnios() -> debe devolver 2
+        a.CumpliAnios() -> debe devolver 3
+    '''
+    #Tu código aca:
+    def __init__(self):
+        self.Edad = 0
+        self.Especie = ''
+        self.Color = ''
+
+    def CumplirAnios(self,especie, color):
+        Edad = Edad +1
---
+++ prueba8.py
@@ -0,0 +1,40 @@
+def NumeroBinario(numero):
+    '''
+    Esta función recibe como parámetro un número entero mayor ó igual a cero y lo devuelve en su 
+    representación binaria. Debe recibir y devolver un valor de tipo entero.
+    En caso de que el parámetro no sea de tipo entero y mayor a -1 debe retornar nulo.
+    Recibe un argumento:
+        numero: Será el número que se convertirá a binario.
+    Ej:
+        NumeroBinario(12) debe retornar 1100
+        NumeroBinario(2) debe retornar 10
+        NumeroBinario(14) debe retornar 1110
+    '''
+    #Tu código aca:
+    if not(isinstance(numero,int)) or numero <= -1:
+        resultado = None
+    else:
+        arreglo = [] 
+        binario = []
+
+        while numero != 0: 
+            residuo = numero % 2
+            cociente = numero // 2
+            arreglo.append(residuo) 
+            numero = cociente 
+
+        new_arreglo = arreglo[:]
+        for i in reversed(new_arreglo):
+            binario.append(i)
+        resultado = "".join([str(i) for i in binario])
+
+    
+    return resultado
+
+print(NumeroBinario(5))
+print(NumeroBinario(255))
+print(NumeroBinario(-10))
+print(NumeroBinario('hey'))
+
+
+
