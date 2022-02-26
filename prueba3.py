b0a499def31909ad39441007dd1fcc59ab87b2ff
Alex <arodriguezv@uni.pe>
2022-02-26 01:49:31 -0500

    prueba n3

--- prueba3.py
+++ prueba3.py
@@ -1,47 +1,23 @@
+
 def NumeroCapicua(numero):
-    '''
-    En matemáticas, la palabra capicúa (del catalán cap i cua, 'cabeza y cola')​ 
-    se refiere a cualquier número que se lee igual de izquierda a derecha que 
-    de derecha a izquierda. Se denominan también números palíndromos.
-    Esta función devuelve el valor booleano True si el número es capicúa, de lo contrario
-    devuelve el valor booleano False 
-    En caso de que el parámetro no sea de tipo entero, debe retornar nulo.
-    Recibe un argumento:
-        numero: Será el número sobre el que se evaluará si es capicúa o no lo es.
-    Ej:
-        NumeroCapicua(787) debe retornar True
-        NumeroCapicua(108) debe retornar False
-    '''
-    #Tu código aca:
+    global total
+    # Tu código aca:
     lista= []
-    if type(numero)=='int':
-        hola = list(str(numero))
-        for i in reversed(hola):
+    if isinstance(numero,int):
+        arreglo = list(str(numero))
+        for i in reversed(arreglo):
             lista.append(i)
         invertido = "".join(lista)
 
-        if(numero == invertido):
+        if(int(invertido) == int(numero)):
             resultado = True
         else:
             resultado = False 
-
-        return resultado    
     else:
-        return 'falta'
-   
+        resultado = None
 
-        
-print(NumeroCapicua(123))    
+    return resultado
 
-
-# def invertir_numero(n):
-#     numero = 0
-#     while n != 0:
-#         numero = 10*numero+n % 10
-#         print(numero)
-#         # print(n)
-#         n //= 10
-#         # print(n)
-#     return numero
-
-# print(invertir_numero(123))
+print(NumeroCapicua(232)) 
+print(NumeroCapicua(132)) 
+print(NumeroCapicua('hola')) 
---
+++ prueba4.py
@@ -0,0 +1,33 @@
+def NumeroCapicua(numero):
+    '''
+    En matemáticas, la palabra capicúa (del catalán cap i cua, 'cabeza y cola')​ 
+    se refiere a cualquier número que se lee igual de izquierda a derecha que 
+    de derecha a izquierda. Se denominan también números palíndromos.
+    Esta función devuelve el valor booleano True si el número es capicúa, de lo contrario
+    devuelve el valor booleano False 
+    En caso de que el parámetro no sea de tipo entero, debe retornar nulo.
+    Recibe un argumento:
+        numero: Será el número sobre el que se evaluará si es capicúa o no lo es.
+    Ej:
+        NumeroCapicua(787) debe retornar True
+        NumeroCapicua(108) debe retornar False
+    '''
+    #Tu código aca:
+    lista= []
+    if isinstance(numero,int):
+        arreglo = list(str(numero))
+        for i in reversed(arreglo):
+            lista.append(i)
+        invertido = "".join(lista)
+
+        if(int(invertido) == int(numero)):
+            resultado = True
+        else:
+            resultado = False 
+    
+
+    return resultado
+   
+
+        
+print(NumeroCapicua(323)) 
